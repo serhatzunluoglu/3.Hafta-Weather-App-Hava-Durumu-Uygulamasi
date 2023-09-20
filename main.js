b0cfa023821a61fdd5b353beef67eafe3fe72043
Serhat Zunluoğlu <97339604+serhatzunluoglu@users.noreply.github.com>
2023-09-20 16:15:37 +0300

    Weather statement has been updated in Turkish

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const setQuery = (e) => {
 }
 
 const getResult=(cityName)=>{
-    let query= `${url}weather?q=${cityName}&appid=${key}&units=metric`
+    let query= `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
     fetch(query)
     .then(weather => {
         return weather.json()
